253743ebc098f5e0f0c41bc94bb391acc3bcc11a
princepansuriya53 <pansuriyaprince53@gmail.com>
2025-06-18 13:52:03 +0530

    Update background image

--- lib/main.dart
+++ lib/main.dart
@@ -56,11 +56,10 @@ class MyGlassWidgetState extends State<MyGlassWidget>
         children: [
           Positioned.fill(
             child: Image.network(
-              'https://images.pexels.com/photos/13750357/pexels-photo-13750357.jpeg',
+              'https://plus.unsplash.com/premium_photo-1670274384528-a841002971e9?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
               fit: BoxFit.cover,
             ),
           ),
-
           AnimatedBuilder(
             animation: jellyController,
             builder: (context, child) {
